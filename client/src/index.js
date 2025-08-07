3a3b5e79325656ed3409160581d7f5d2d38a5639
hoonl <akbarrezaie6@gmail.com>
2025-08-07 16:15:22 +0200

    Frontend started

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import "./css/style.css"
+
+const modal = document.getElementById("modal")
+const modalBtn = document.getElementById("modal-btn")
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv").config()
 const port = process.env.PORT || 5000
 const connectDB = require("./config/db")
-
+const path = require("path")
 
 // Connect to MongoDB
 connectDB()
@@ -12,6 +12,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Serve static files from the React app
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "public")))
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"))
+// })
+// }
 
 app.get("/", (req, res) => {
   res.json({
@@ -19,10 +26,8 @@ app.get("/", (req, res) => {
   })
 })
 
-
 const ideasRouter = require("./routes/ideas")
 app.use("/api/ideas", ideasRouter)
-
 
 app.listen(port, () =>
   console.log(`Server is running on http://localhost:${port}`)
